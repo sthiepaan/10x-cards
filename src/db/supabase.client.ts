1fa865f595ea7cc1385884fe4a92b92901b16f6c
sthiepaan <info@damianszczypka.com>
2025-10-13 21:16:34 +0200

    10X-2x4 Changes for '10x-cards' Project
    - Add 'SupabaseClient' Type in 'Supabase' Client
    - Add 'DEFAULT_USER_ID' Constant in 'Supabase' Client

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -6,3 +6,7 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export type SupabaseClient = typeof supabaseClient;
+
+export const DEFAULT_USER_ID = "e87a1f38-c40a-4628-90ff-b285a7672105";
